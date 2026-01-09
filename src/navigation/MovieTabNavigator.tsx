789d7017b3ca20d77cf902a6528963d9b5fa4a64
Pranav Nanegaonkar <nanegaonkarpranav68@gmail.com>
2026-01-09 09:30:41 +0530

    cc

--- src/navigation/MovieTabNavigator.tsx
+++ src/navigation/MovieTabNavigator.tsx
@@ -1,5 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+type BottomTabProps = {
+  Home: undefined;
+  Profile: undefined;
+  Menu: undefined;
+};
+
+const BottomTabs = createBottomTabNavigator();
 
 export default function MovieTabNavigator() {
   return (
