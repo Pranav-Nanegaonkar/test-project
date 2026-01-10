9221a708fc00ce548038e24dbb903b638b74b853
Pranav Nanegaonkar <nanegaonkarpranav68@gmail.com>
2026-01-10 17:27:41 +0530

    cc

---
+++ src/screens/CameraVisionDemo.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import CustomHeader from '../components/CustomHeader';
+
+export default function CameraVisionDemo() {
+  return (
+    <>
+      <CustomHeader title="ReduxDemo" />
+      <View>
+        <Text>CameraVisionDemo</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
--- src/screens/ReduxDemo.tsx
+++ src/screens/ReduxDemo.tsx
@@ -12,6 +12,7 @@ import { useAppDispatch, useAppSelector } from '../hooks/reduxHooks';
 import { login, logout } from '../features/auth/authSlice';
 import UserCard from '../components/UserCard';
 import { getUsersAction } from '../features/test/testSlice';
+import CustomHeader from '../components/CustomHeader';
 
 export default function ReduxDemo() {
   const dispatch = useAppDispatch();
@@ -25,45 +26,50 @@ export default function ReduxDemo() {
   console.log(users);
 
   return (
-    <View className=" flex-1 items-center justify-center  gap-y-5">
-      <TouchableOpacity
-        className=" w-40 bg-[#2196f3] rounded-lg"
-        onPress={() =>
-          dispatch(login({ email: 'test@mail.com', password: '1234' }))
-        }
-      >
-        <Text className="text-2xl font-bold text-white text-center">Login</Text>
-      </TouchableOpacity>
-      <View>
-        {loading && <ActivityIndicator size={30} color="#2196f3" />}
-        {user && <Text>Welcome {user.email}</Text>}
+    <>
+      <CustomHeader title="ReduxDemo" />
+      <View className=" flex-1 items-center justify-center  gap-y-5">
+        <TouchableOpacity
+          className=" w-40 bg-[#2196f3] rounded-lg"
+          onPress={() =>
+            dispatch(login({ email: 'test@mail.com', password: '1234' }))
+          }
+        >
+          <Text className="text-2xl font-bold text-white text-center">
+            Login
+          </Text>
+        </TouchableOpacity>
+        <View>
+          {loading && <ActivityIndicator size={30} color="#2196f3" />}
+          {user && <Text>Welcome {user.email}</Text>}
+        </View>
+        <TouchableOpacity
+          className=" w-40 bg-[#2196f3] rounded-lg"
+          onPress={() => dispatch(logout())}
+        >
+          <Text className="text-2xl font-bold text-white text-center ">
+            Logout
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          className=" w-40 bg-[#2196f3] rounded-lg"
+          onPress={() => dispatch(getUsersAction())}
+        >
+          <Text className="text-2xl font-bold text-white text-center ">
+            Get Users
+          </Text>
+        </TouchableOpacity>
+
+        {uLoading && <ActivityIndicator size={30} color="#2196f3" />}
+        {users && <Text>{users.length}</Text>}
+
+        <FlatList
+          data={users}
+          renderItem={({ item }) => <UserCard user={item} />}
+          keyExtractor={item => item.id.toString()}
+        />
       </View>
-      <TouchableOpacity
-        className=" w-40 bg-[#2196f3] rounded-lg"
-        onPress={() => dispatch(logout())}
-      >
-        <Text className="text-2xl font-bold text-white text-center ">
-          Logout
-        </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        className=" w-40 bg-[#2196f3] rounded-lg"
-        onPress={() => dispatch(getUsersAction())}
-      >
-        <Text className="text-2xl font-bold text-white text-center ">
-          Get Users
-        </Text>
-      </TouchableOpacity>
-
-      {uLoading && <ActivityIndicator size={30} color="#2196f3" />}
-      {users && <Text>{users.length}</Text>}
-
-      <FlatList
-        data={users}
-        renderItem={({ item }) => <UserCard user={item} />}
-        keyExtractor={item => item.id.toString()}
-      />
-    </View>
+    </>
   );
 }
