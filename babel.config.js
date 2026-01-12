460cc67c6cbbec12f64ada491e1a6d6d7a5ad8de
Pranav Nanegaonkar <nanegaonkarpranav68@gmail.com>
2026-01-12 18:27:58 +0530

    Your commit message

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,15 @@
-const workletsPluginOptions = {
-  // Your custom options.
-};
 
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
   plugins: [
+<<<<<<< Updated upstream
     [
       'react-native-reanimated/plugin',
       'react-native-worklets/plugin',
       workletsPluginOptions,
     ],
+=======
+    ['react-native-worklets/plugin', 'react-native-worklets-core/plugin'],
+>>>>>>> Stashed changes
   ],
 };
--- src/navigation/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator.tsx
@@ -12,7 +12,27 @@ import MovieTabNavigator from './MovieTabNavigator';
 import ReduxDemo from '../screens/ReduxDemo';
 import CameraScreen from '../screens/CameraScreen';
 
+<<<<<<< Updated upstream
 const Drawer = createDrawerNavigator();
+=======
+export type DrawerNavigationList = {
+  Menu: undefined;
+  ImagePickerDemo: undefined;
+  DocumentPickerDemo: undefined;
+  DropDownDemo: undefined;
+  VectorIconDemo: undefined;
+  Test: undefined;
+  MovieTabNavigator: undefined;
+  ReactReduxDemo: undefined;
+  CameraVisionDemo: undefined;
+  CameraVisionDemoCapturePhoto: undefined;
+  CameraVisionDemoZoom: undefined;
+  PreviewScreen: undefined;
+  CameraStackNavigator: undefined;
+};
+
+const Drawer = createDrawerNavigator<DrawerNavigationList>();
+>>>>>>> Stashed changes
 
 export default function DrawerNavigator() {
   return (
@@ -23,7 +43,11 @@ export default function DrawerNavigator() {
         headerShown: false,
       }}
       // remove this after done
+<<<<<<< Updated upstream
       initialRouteName="CameraVision"
+=======
+      initialRouteName="CameraStackNavigator"
+>>>>>>> Stashed changes
     >
       <Drawer.Screen name="Menu" component={TabNavigator} />
       <Drawer.Screen name="ImagePickerDemo" component={ImagePickerDemo} />
@@ -33,7 +57,20 @@ export default function DrawerNavigator() {
       <Drawer.Screen name="Test" component={Test} />
       <Drawer.Screen name="MovieTabNavigator" component={MovieTabNavigator} />
       <Drawer.Screen name="ReactReduxDemo" component={ReduxDemo} />
+<<<<<<< Updated upstream
       <Drawer.Screen name="CameraScreen" component={CameraScreen} />
+=======
+      <Drawer.Screen name="CameraVisionDemo" component={CameraVisionDemo} />
+      <Drawer.Screen
+        name="CameraStackNavigator"
+        component={CameraStackNavigator}
+      />
+      <Drawer.Screen
+        name="CameraVisionDemoZoom"
+        component={CameraVisionDemoZoom}
+      />
+      <Drawer.Screen name="PreviewScreen" component={PreviewScreen} />
+>>>>>>> Stashed changes
     </Drawer.Navigator>
   );
 }
